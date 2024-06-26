dae9c6754ebae28099cb947f88d7dd46b6d9aab2
Utkarsh <vermau2k01@gmail.com>
2024-06-26 09:25:25 +0530

    Initial Commit

---
+++ src/test/java/com/vermau2k01/scm20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermau2k01.scm20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
