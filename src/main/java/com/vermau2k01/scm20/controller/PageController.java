0e04d165638679fd63969c6cea7ec4e3d6bcc5b8
Utkarsh <vermau2k01@gmail.com>
2024-06-27 18:29:42 +0530

    Working on nav bar

--- src/main/java/com/vermau2k01/scm20/controller/PageController.java
+++ src/main/java/com/vermau2k01/scm20/controller/PageController.java
@@ -1,21 +1,48 @@
 package com.vermau2k01.scm20.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+
+
 @Controller
 public class PageController {
     
-    private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+   
     
     @RequestMapping("/home")
     public String home(Model model) {
-        logger.info("Home controller");
+       
         model.addAttribute("name", "substring technology");
         model.addAttribute("project", "smart contact manager");
         return "home";
     }
+
+    // about page
+    @RequestMapping("/about")
+    public String about(Model model) {
+        System.out.println("About page");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String Services() {
+        return "services";
+    }
+
+    
+    @RequestMapping("/contact")
+    public String Contacts() {
+        return "contact";
+    }
+
+    @RequestMapping("/login")
+    public String Login() {
+        return "login";
+    }
+    
 }
